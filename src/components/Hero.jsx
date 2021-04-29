c6882de824542a83b5e2b6a2c8253f90164f58cb
phongduybui <buiduyphong921@gmail.com>
2021-04-29 21:32:06 +0700

    fix hero cover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,23 @@
 /* eslint-disable */
 import React from 'react';
 import './Hero.scss';
+import cover from '../images/illustration-working.svg';
 import FormInput from './FormInput';
 
 const Hero = () => {
   return (
     <section className="hero">
-        <div className="hero-content">
-            <h1 className="hero-content__title">
-                More than just shorter links
-            </h1>
-            <p className="hero-content__desc">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus et id at.
-            </p>
-            <button className="hero-content__btn btn btn--rounded">Get Started</button>
-        </div>
-        <FormInput />
+      <img src={cover} className="hero__cover"></img>
+      <div className="hero__content">
+          <h1 className="hero__title">
+              More than just shorter links
+          </h1>
+          <p className="hero__desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus et id at.
+          </p>
+          <button className="hero__btn btn btn--rounded">Get Started</button>
+      </div>
+      <FormInput />
     </section>
   );
 };
