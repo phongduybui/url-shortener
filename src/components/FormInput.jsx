17bc4ec5201fb4595949efd478fd98a14bee553e
phongduybui <buiduyphong921@gmail.com>
2021-04-29 21:53:50 +0700

    edit input form position

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -5,8 +5,11 @@ import './FormInput.scss';
 const FormInput = () => {
   return (
     <form className="form-input">
-      <div className="wrap">
-        <input className="input-link"></input>
+      <div className="form-input__wrapper">
+        <input 
+          className="form-input__input"
+          placeholder="Shorten a link here!"
+        ></input>
         <button className="btn btn--rounded">Shorten </button>
       </div>
     </form>
