530266858f984ce72e5206ad3852fb8e6c33defd
phongduybui <buiduyphong921@gmail.com>
2021-05-01 23:10:56 +0700

    add intro component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Header from './Header';
 import Hero from './Hero';
+import Intro from './Intro';
 
 const App = () => (
   <div className="app">
     <Header />
     <Hero />
+    <Intro />
   </div>
 );
 
