2a62856f94a126695242063f3c6ce550df721cc0
phongduybui <buiduyphong921@gmail.com>
2021-05-02 21:56:59 +0700

    update card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,11 @@ import './Card.scss';
 const Card = () => {
     return (
         <div className="card">
-            
+            <div className="card__icon">
+                <i className="icon history"></i>
+            </div>
+            <h3 className="card__title">Brand Recognition</h3>
+            <p className="card__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, voluptatem eum. Cumque, non atque tenetur tempora pariatur neque fuga voluptate.</p>
         </div>
     );
 };
