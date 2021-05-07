27eb3b578a8ae89dc2dfbc055ea00a51c3826728
phongduybui <buiduyphong921@gmail.com>
2021-05-07 23:57:22 +0700

    responsive Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
               More than just shorter links
           </h1>
           <p className="hero__desc">
-            Build your brand's recognition and get detailed insights on how your links arc pcrforming.
+            Build your brand's recognition and get detailed insights on how your links are performing.
           </p>
           <button className="btn btn--rounded btn--large">
             <span>Get Started</span>
