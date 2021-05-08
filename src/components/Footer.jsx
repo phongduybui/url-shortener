260b1888f42e666af5ed5c024d0916b0c2149bc2
phongduybui <buiduyphong921@gmail.com>
2021-05-08 20:06:49 +0700

    scrollIntoView FormInput component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Footer = () => {     
+const Footer = ({ formRef }) => {     
     return (
         <footer className="footer">
             <h2 className="footer__title">Boost your links today</h2>
-            <button className="btn btn--rounded btn--large">
+            <div 
+                className="btn btn--rounded btn--large"
+                onClick={() => formRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' })}
+            >
                 <span>Get Started</span>
-            </button>
+            </div>
         </footer>
     );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable */
 import React from 'react';
 import cover from '../images/illustration-working.svg';
-import FormInput from './FormInput';
 
-const Hero = () => {
+const Hero = ({ formRef }) => {
   return (
     <section className="hero">
       <img src={cover} className="hero__cover"></img>
@@ -14,11 +13,13 @@ const Hero = () => {
           <p className="hero__desc">
             Build your brand's recognition and get detailed insights on how your links are performing.
           </p>
-          <button className="btn btn--rounded btn--large">
+          <button 
+            className="btn btn--rounded btn--large"
+            onClick={() => formRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' })}
+          >
             <span>Get Started</span>
           </button>
       </div>
-      <FormInput />
     </section>
   );
 };
