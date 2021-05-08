9fc0ea195499b10fa3807ad7f6bfcaa80fa4bb49
phongduybui <buiduyphong921@gmail.com>
2021-05-08 11:15:44 +0700

    validate form input

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,24 +1,39 @@
 /* eslint-disable */
 import React from 'react';
 import bitly from '../apis/bitly';
+import { useForm } from 'react-hook-form';
 
 const FormInput = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const dataString = '{"long_url": "https://stackoverflow.com/questions/47630163/axios-post-request-to-send-form-data"}'; 
+  const onSubmit = ({ link }) => {
+    const dataString = `{"long_url": "${link}"}`; 
     bitly.post('/shorten', dataString)
       .then(response => console.log(response))
   }
 
+  console.log(errors)
   return (
-    <form onSubmit={handleSubmit} className="form-input">
+    <form onSubmit={handleSubmit(onSubmit)} className="form-input">
       <div className="form-input__wrapper">
-        <input 
-          className="form-input__input"
-          placeholder="Shorten a link here..."
-        ></input>
+        <div className="form-input__inner">
+          <input
+            {...register('link', { 
+              required: {
+                value: true,
+                message: 'Please add a link'
+              },
+              pattern: {
+                value: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm,
+                message: 'Invalid URL'
+              }
+            })}
+            className={`form-input__input ${errors.link && 'error'}`}
+            placeholder="Shorten a link here..."
+          />
+          {errors.link && <p className="error-message">{errors.link.message}</p>}
+        </div>
         <button className="form-input__btn btn btn--rounded">
           <span>Shorten It!</span>
         </button>
