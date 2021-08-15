1e8c5e1534e4e08dca329b8fceb1641ea490ea8c
Phong Duy Bui <buiduyphong921@gmail.com>
2021-08-15 20:09:09 +0700

    Fix click navlink make page reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 
 const Navbar = ({ collapse }) => {
-    return (
-        <div className={`navbar ${collapse ? 'navbar--collapse' : ''}`}>
-            <ul className="navbar__menu">
-                <li className="nav-item">
-                <a href="/#" className="nav-link active">Features</a>
-                </li>
-                <li className="nav-item">
-                <a href="/#" className="nav-link">Pricing</a>
-                </li>
-                <li className="nav-item">
-                <a href="/#" className="nav-link">Resources</a>
-                </li>
-            </ul>
-            <div className="navbar__button">
-                <button className="btn btn--no-bg"><span>Login</span></button>
-                <button className="btn btn--rounded"><span>Sign Up</span></button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`navbar ${collapse ? 'navbar--collapse' : ''}`}>
+      <ul className='navbar__menu'>
+        <li className='nav-item'>
+          <a href='javascript:void(0)' className='nav-link active'>
+            Features
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='javascript:void(0)' className='nav-link'>
+            Pricing
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='javascript:void(0)' className='nav-link'>
+            Resources
+          </a>
+        </li>
+      </ul>
+      <div className='navbar__button'>
+        <button className='btn btn--no-bg'>
+          <span>Login</span>
+        </button>
+        <button className='btn btn--rounded'>
+          <span>Sign Up</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
